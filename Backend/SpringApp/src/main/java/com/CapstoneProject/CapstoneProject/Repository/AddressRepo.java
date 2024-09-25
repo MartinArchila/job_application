d20d96097552513b778643f38cddbc31a8be12e0
marc0an1 <81052456+marc0an1@users.noreply.github.com>
2024-09-25 15:19:36 -0400

    Address Repo created and tested

---
+++ Backend/SpringApp/src/main/java/com/CapstoneProject/CapstoneProject/Repository/AddressRepo.java
@@ -0,0 +1,8 @@
+package com.CapstoneProject.CapstoneProject.Repository;
+
+import com.CapstoneProject.CapstoneProject.Model.Address;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepo extends CrudRepository<Address, Integer> {
+
+}
